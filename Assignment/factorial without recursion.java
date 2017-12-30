ba850add4a65d511334511553c2b84d44ba7ef04
swarnabarathik <34641522+swarnabarathik@users.noreply.github.com>
2017-12-30 20:51:45 +0530

    Create factorial without recursion.java

---
+++ Assignment/factorial without recursion.java
@@ -0,0 +1,12 @@
+class factorial
+{
+	public static void main(String args[])
+	{
+		int num=5,fact=1;
+		for(int i=1;i<=num;i++)
+		{
+			fact =fact*i;
+		}
+		System.out.println("Factorial is"+fact);
+	}
+}
