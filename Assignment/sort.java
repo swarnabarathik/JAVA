6f0f15554368487b868143a600b156e2957a1cb1
swarnabarathik <34641522+swarnabarathik@users.noreply.github.com>
2017-12-30 21:43:18 +0530

    Create sort.java

---
+++ Assignment/sort.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class sort
+{
+	public static void main (String args[]) 
+	{
+	
+		int[] a={ 3,5,2,7,9,1};
+		Arrays.sort(a);
+		System.out.println("Modified array:"+Arrays.toString(a));
+	}
+}
